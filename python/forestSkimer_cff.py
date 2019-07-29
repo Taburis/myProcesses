fa13f797581fce1f2eb2bdda91162e09dc65f033
kumarv <vineet.kumar@cern.ch>
2019-07-29 18:39:07 +0200

    extended Xiao code to add Jet variables

--- python/forestSkimer_cff.py
+++ python/forestSkimer_cff.py
@@ -2,8 +2,9 @@ import FWCore.ParameterSet.Config as cms
 from HeavyIonsAnalysis.PhotonAnalysis.ggHiNtuplizer_cfi import ggHiNtuplizer
 
 forestSkim = cms.EDAnalyzer(
-    	"forestSkimer",
-	doMuon = cms.bool(True)
+    "forestSkimer",
+    doMuon = cms.bool(False),
+    doJets = cms.bool(False)
 )
 
 ggHiNtuplizer.doMuons = forestSkim.doMuon
