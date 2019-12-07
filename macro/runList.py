de2007274eb99b0c3d2cea93e49acfe38f51b540
Taburis <xwang241@uic.edu>
2019-12-07 06:05:19 +0100

    merge, drop local change

--- macro/runList.py
+++ macro/runList.py
@@ -8,15 +8,17 @@ listpath = 'DBList/list_2018AA_dijet_PH_Inna.txt'
 output = 'AA2018bTagger_dijetMC_HP'
 doMerge = 1
 listf = open(listpath).readlines()
-
 istart = 0
+outputlist = []
 for fs in listf :
 	outputname = output+'_id'+str(istart)
+	outputlist.append(outputname)
 	istart+=1
 	cmdline = ['root', '-l', '-b', '-q', execf+'(\"'+fs.rstrip()+'\", \"'+outputname+'\")']
+	outputlist[-1]=outputlist[-1]+'_step1.root'
 	subprocess.call(cmdline)
-
 if doMerge:
-	cmdline['hadd', '-f', output+'.root', output+'_id*']
+	cmdline = ['hadd', '-f', output+'_step1.root']+outputlist
+	print cmdline
 	subprocess.call(cmdline)
 print 'done!'
