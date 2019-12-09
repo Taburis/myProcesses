369ea9034f3a6510ee11dfa4d2580cf50c23b804
Xiao Wang <xwang241@uic.edu>
2019-12-08 21:46:51 -0600

    merge

--- macro/taggerStep2Config.C
+++ macro/taggerStep2Config.C
@@ -3,8 +3,8 @@
 #include "config_aa2018_bjet.h"
 
 void taggerStep2Config(){
-	auto fMC = TFile::Open("../macro/AA2018bTagger_dijetMC_HP_step1.root");
-	auto fData = TFile::Open("../macro/AA2018bTagger_DataJet80_HardprobDB_step1.root");
+	auto fMC = TFile::Open("AA2018bTagger_dijetMC_HP_step1.root");
+	auto fData = TFile::Open("AA2018bTagger_DataJet80_HardprobDB_step1.root");
 	using namespace AA2018bJet;
 	
 	auto btagger  = new bTaggerStep2Analyzer("AA2018bTagger");
