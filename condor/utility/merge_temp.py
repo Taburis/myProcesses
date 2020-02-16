b3b65bfc53143e3ae839a4fdd0ca5cfd0ddee16b
Taburis <xwang241@uic.edu>
2020-02-16 19:54:26 +0100

    fixed a bug in last merge

--- condor/utility/merge_temp.py
+++ condor/utility/merge_temp.py
@@ -8,11 +8,11 @@ INPUT
 output = OUTPUT
 
 
-for f in listf:
+for f in inlist:
 	if not os.path.exists(f): 
 		print 'Abort: waiting for all jobs done.'
 		sys.exit()
 
 print 'jobs done, starting the last merge step...'
-cmd = ['hadd','-f', out]+listf
+cmd = ['hadd','-f', out]+inlist
 subprocess.call(cmd)
