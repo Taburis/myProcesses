b37301c0b3ff0af57c14259151b6f59ccc636525
Taburis <xwang241@uic.edu>
2020-05-07 22:24:43 +0200

    merge attempt

--- jtc/bjet2018/step1/crab_djetMC.py
+++ jtc/bjet2018/step1/crab_djetMC.py
@@ -12,7 +12,8 @@ config.section_("Site")
 
 #------------------config ----------------------
 config.JobType.maxJobRuntimeMin =150 
-config.General.requestName = 'bjtc_aa2018_dijetP8H_drum_format2_std_run1'
+config.General.requestName = 'bjtc_aa2018_dijetP8H_drum_format2_sube_run1'
+#config.General.requestName = 'bjtc_aa2018_dijetP8H_drum_format2_std_run1'
 #fileList = '../dblist/list_dijet_hiforest_Jussi_forTesting.txt'
 fileList = '../dblist/list_djetMC_skim_prdue_run1.txt'
 
