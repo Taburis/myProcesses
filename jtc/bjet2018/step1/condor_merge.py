f853ba4865432c4ec2065511f8610089fdca4ab9
Taburis <xwang241@uic.edu>
2020-05-15 21:59:03 +0200

    updated the db list

--- jtc/bjet2018/step1/condor_merge.py
+++ jtc/bjet2018/step1/condor_merge.py
@@ -1,9 +1,11 @@
 
 import myProcesses.condor.merge_job as mj
-#job_name = "bjtc_bjet_standard_run1"
+job_name = "bjtc_djet_std_v2"
+#job_name = "bjtc_djet_sube_v2"
+#job_name = "bjtc_bjet_std_v2"
 #job_name = "bjtc_bjet_sube_run1"
-job_name = "bjtc_data_hardproe"
-inputf = "/eos/cms/store/group/phys_heavyions/wangx/aa2018bjet/crab_bjtc_aa2018_hardprobe_data_50mix_run1/200512_155936/"
+#job_name = "bjtc_data_hardproe_v2"
+inputf = "/eos/cms/store/group/phys_heavyions/wangx/aa2018bjet/crab_bjtc_aa2018_dijetP8H_drum_std_v2"
 #job_name = "bjtc_bjetMC_drum_skim"
 #job_name = "bjtc_bjetMC_drum_skim"
 #inputf = "/eos/cms/store/group/phys_heavyions/wangx/aa2018bjet/crab_bjtc_aa2018_bijetP8H_drum_test/"
