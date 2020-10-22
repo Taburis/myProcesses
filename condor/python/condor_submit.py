b20028af53fa0305f90769aca937a8942496e0c4
Tabris <tabris.xiao@gmail.edu>
2020-10-22 18:10:39 -0500

    improved the condor job manager

---
+++ condor/python/condor_submit.py
@@ -0,0 +1,28 @@
+
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-p', dest='path', help='path to search')
+parser.add_argument('-c', dest='cfg', help='config')
+args = parser.parse_args()
+inpath = args.path
+cfg = args.cfg
+
+os.chdir(inpath)
+
+flist = []
+files = os.listdir(inpath)
+
+for f in files:
+        if f.split(cfg)[0] != '':
+            continue
+	flist.append(f)
+
+
+for f in flist:
+	os.system("condor_submit {CFG}".format(CFG=f))
+
+
+# this is the script to submit jobs for lpc condor system.
